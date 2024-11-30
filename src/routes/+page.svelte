c9f704213ebf8a971e0acc5d744242693e265798
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-11-30 19:02:11 +0200

    Added some stylization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1>Welcome to The Blind Bit Boyz Crypto initiative! </h1>
+<h1 class="text-3xl sans-serif font-semibold text-blue-900 text-center uppercase">Welcome to The Blind Bit Boyz Crypto initiative! </h1>
 
-<p>This is a website that is designed to bring accessibility to the ever expanding cripto market for blind users.</p>
+<body>
+<p class="xl sans-serif text-blue-600  text-center ">
+        This is a website that is designed to bring accessibility to the ever expanding cripto market for users who are blind.</p>
+<p class="xl sans-serif text-blue-600  text-center ">
+	A Proud Product developed in partnership with :
+
+<a class="text-gold underline hover: text-gold-dark" href="https://envisionly.tech">Envisionly</a>
+
+<a class="text-gold underline hover: text-gold-dark" href="https://www.google.com/wp/en/</p>">And Yeatersink.</a>
+</p>
+</body>
+
