fbbf018c824a00d3c4dc37cbdfbe1e0dd1726ad1
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-11-30 13:07:49 +0200

    Added API info for DEX Screener

--- src/components/DesktopNav.svelte
+++ src/components/DesktopNav.svelte
@@ -1,5 +1,6 @@
 <nav>
 <ul>
 <li><a href="/">Home</a></li>
+<li><a href="/chart-analyzer">Chart Analyzer</a></li>
 </ul>
     </nav>
---
+++ src/routes/chart-analyzer/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { onMount } from 'svelte';
+    let query = '';
+    let results = [];
+
+    async function searchCryptocurrencies() {
+        const response = await fetch(`https://api.dexscreener.io/latest/dex/search/?q=${query}`);
+        const data = await response.json();
+        results = data.pairs;
+    }
+</script>
+
+
+<svelte:head>
+<title>Welcome to the Chart Analyzer.</title>
+</svelte:head>
+
+<h1> Chart Analyzer</h1>
+<form on:submit|preventDefault={searchCryptocurrencies}>
+    <label for="provider">Choose Your Data Provider</label>
+    <select id="provider">
+        <option value="dexscreener">Dex Screener</option>
+        <option disabled value="dextools">Dex Tools</option>
+        <option disabled value="coingecko">Coin Gecko</option>
+        <option disabled value="coinbase">Coin Base</option>
+    </select>
+    <br>
+    <label for="search">Search Cryptocurrencies</label>
+    <input id="search" type="text" bind:value={query} placeholder="Enter cryptocurrency name">
+    <button type="submit">Search</button>
+</form>
+
+{#if results.length > 0}
+	<h2>Results: {results.length}</h2>
+    <ul>
+        {#each results as result}
+            <li>{result.baseToken.name} ({result.baseToken.symbol}) {result.baseToken.address} / {result.quoteToken.name} ({result.quoteToken.symbol}) / {result.pairAddress} ${result.priceUsd}</li>
+        {/each}
+    </ul>
+{/if}
