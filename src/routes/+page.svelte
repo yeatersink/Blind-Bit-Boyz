e3b6998e4cac65b354df3a2d02df6a0cd58334ad
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-11-30 20:58:40 +0200

    Made some changes to style.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1 class="text-3xl sans-serif font-semibold text-blue-900 text-center uppercase">Welcome to The Blind Bit Boyz Crypto initiative! </h1>
 
-<body>
+
 <p class="xl sans-serif text-blue-600  text-center ">
         This is a website that is designed to bring accessibility to the ever expanding cripto market for users who are blind.</p>
 <p class="xl sans-serif text-blue-600  text-center ">
@@ -10,5 +10,5 @@
 
 <a class="text-gold underline hover: text-gold-dark" href="https://www.google.com/wp/en/</p>">And Yeatersink.</a>
 </p>
-</body>
+
 
