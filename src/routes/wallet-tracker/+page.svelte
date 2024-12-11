7bb8dca5dac2979758460fc0b3e64f296b107614
yeatersink <163296549+yeatersink@users.noreply.github.com>
2024-12-11 15:06:33 +0200

    Added wallet tracker page.

---
+++ src/routes/wallet-tracker/+page.svelte
@@ -0,0 +1,17 @@
+<h1>Wallet Tracker</h1>
+<p>So you don't have a life at all and you want to be nosey and track someones wallet? Maybe you are a good person, but you want to try to get the jump on your favorite influencer? Maybe you want to try to find out which wallet holds the most coins before you ape in? Thats a good idea.</p>    
+<p>This tool will allow you to track a contract address to find out the following:</p> 
+<ol> 
+1 Search for a contract address.
+2 Track the Wallet which owns the coin searched.
+</ol>
+<p>To use this tool, simply put the contract address of the coin that you want to track. Then press the search button.</p>
+<p>After the results appear, just click on the name of the coin and a new tab will open with a list of the wallett addresses which currently own that coin. </p>
+<p>To see other coins owned by that wallet, simply click on the wallet address, and a new tab will open which shows you a list of coins owned by that wallet.</p>
+<h1>Coin Search</h1>
+<form>
+    <label for="search">Search Cryptocurrencies</label>
+	<input id="search" type="search" placeholder="Enter cryptocurrency name" />
+	<br />
+	<button type="submit">Search</button>
+</form>
