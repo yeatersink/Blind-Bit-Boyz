8c7d2116479a55490b1bcc7f0b5ae3a306fd0fb2
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2024-12-12 15:18:53 +0000

    Finished switching to dex screener

--- src/routes/coins/[address]/+page.server.ts
+++ src/routes/coins/[address]/+page.server.ts
@@ -1,16 +1,17 @@
-
 /** @type {import('./$types').PageServerLoad} */
-export const load = async ({ params }) => {
+export const load = async ({ params, url }) => {
 	const coinAddress = params.address;
-//gets chainid and pairid from url
-const chainId = params.chainid;
-const pairAddress = params.pairid;
+	//gets chainid and pairid from url params
+	const chainId = url.searchParams.get('chainid');
+	const pairAddress = url.searchParams.get('pairid');
 
-const response = await fetch(`https://api.dexscreener.com/latest/dex/pairs/${chainId}/${pairAddress}`);
+	const response = await fetch(
+		`https://api.dexscreener.com/latest/dex/pairs/${chainId}/${pairAddress}`
+	);
 	if (!response.ok) {
 		return { status: response.status };
 	}
 	const data = await response.json();
 	const pair = data.pairs[0];
-	return {status: 200, pair};
-}
+	return { status: 200, pair };
+};
--- src/routes/coins/[address]/+page.svelte
+++ src/routes/coins/[address]/+page.svelte
@@ -1,46 +1,75 @@
 <script lang="ts">
-	let currentTab:string=$state('panel-coin-details');
+	let currentTab: string = $state('panel-coin-details');
 
-	const {data} = $props()
+	const { data } = $props();
 
-	function formatNumberWithDecimals(number:string, decimals:string) {
-		    return (parseInt(number)/(10**parseInt(decimals))).toFixed(parseInt(decimals))
+	function formatNumberWithDecimals(number: string, decimals: string) {
+		return (parseInt(number) / 10 ** parseInt(decimals)).toFixed(parseInt(decimals));
 	}
 </script>
 
 <svelte:head>
 	{#if data.pair}
-		<title>{data.pair.baseToken.name}</title>
+		<title>{data.pair.baseToken.name} ({data.pair.baseToken.symbol})</title>
 	{/if}
 </svelte:head>
 
 {#if data.pair}
 	<h1>{data.pair.baseToken.name}</h1>
+	<p>{data.pair.baseToken.address}</p>
+	<button onclick={() => navigator.clipboard.writeText(data.pair.baseToken.address)}
+		>Copy Address</button
+	>
+	<div>
+		<section role="tablist" class="flex">
+			<button
+				aria-selected={currentTab === 'panel-coin-details'}
+				aria-controls="panel-coin-details"
+				tabindex={currentTab === 'panel-coin-details' ? 0 : -1}
+				role="tab"
+				onclick={() => (currentTab = 'panel-coin-details')}>Details</button
+			>
+			<button
+				aria-selected={currentTab === 'panel-technical-analysis'}
+				aria-controls="panel-technical-analysis"
+				tabindex={currentTab === 'panel-technical-analysis' ? 0 : -1}
+				role="tab"
+				onclick={() => (currentTab = 'panel-technical-analysis')}>Technical Analysis</button
+			>
+		</section>
+	</div>
 
-		<div>
-			<section role="tablist" class="flex">
-					<button
-						aria-selected={currentTab==='panel-coin-details'}
-						aria-controls="panel-coin-details"
-						tabindex={currentTab==='panel-coin-details'?0:-1}
-						role="tab"
-onclick={() => currentTab='panel-coin-details'}
->Details</button
-					>
-					<button
-						aria-selected={currentTab==='panel-technical-analysis'}
-						aria-controls="panel-technical-analysis"
-						tabindex={currentTab==='panel-technical-analysis'?0:-1}
-						role="tab"
-onclick={() => currentTab='panel-technical-analysis'}
->Technical Analysis</button
-					>
-			</section>
-		</div>
-
-<div class={`${currentTab!='panel-coin-details'?'hidden':''}`} role="tabpanel" id="panel-coin-details" aria-label="Coin Details">
-</div>
-<div class={`${currentTab!='panel-technical-analysis'?'hidden':''}`} role="tabpanel" id="panel-technical-analysis" aria-label="Technical Analysis">
-<p>Chart Goes Here</p>
-</div>
+	<div
+		class={`${currentTab != 'panel-coin-details' ? 'hidden' : ''}`}
+		role="tabpanel"
+		id="panel-coin-details"
+		aria-label="Coin Details"
+	>
+		<h2>Token Details</h2>
+		<p>Symbol: {data.pair.baseToken.symbol}</p>
+		<p>Chain: {data.pair.chainId}</p>
+		<h2>Pair Details</h2>
+		<p>Pair Address: {data.pair.pairAddress}</p>
+		<button onclick={() => navigator.clipboard.writeText(data.pair.pairAddress)}
+			>Copy Address</button
+		>
+		<p>Pair created on: {new Date(data.pair.pairCreatedAt)}</p>
+		<p>Paired with: {data.pair.quoteToken.name}</p>
+		<p>Paired token address: {data.pair.quoteToken.address}</p>
+		<button onclick={() => navigator.clipboard.writeText(data.pair.quoteToken.address)}
+			>Copy Address</button
+		>
+		<p>Paired token symbol: {data.pair.quoteToken.symbol}</p>
+		<h2>Price Details</h2>
+		<p>${data.pair.priceUsd}</p>
+		<p>Native: {data.pair.priceNative}</p>
+	</div>
+	<div
+		class={`${currentTab != 'panel-technical-analysis' ? 'hidden' : ''}`}
+		role="tabpanel"
+		id="panel-technical-analysis"
+		aria-label="Technical Analysis"
+	>
+		<p>Chart Goes Here</p>
+	</div>
 {/if}
