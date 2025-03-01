25bfefded9e9a1f3104717682b1ac14ba62b2777
daniel <danielfreeman03@gmail.com>
2025-03-01 10:36:31 -0600

    Added uniswap interface file and npm packages

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,26 @@
 // place files you want to import through the `$lib` alias in this folder.
+document.addEventListener('DOMContentLoaded', function () {
+    const chart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
