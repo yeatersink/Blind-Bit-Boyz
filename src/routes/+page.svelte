f8ab1878ca260da2fc131599f296708f06e33f4a
Paul Geoghegan <mrpaultdgeoghegan@gmail.com>
2025-04-23 17:52:54 +0100

    Added under construction messages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">Welcome to The Blind Bit Boyz!</h1>
+<h2 class="text-xl font-medium">Under Construction</h2>
+<p>We are currently working on our website. Please check back later for updates.</p>
