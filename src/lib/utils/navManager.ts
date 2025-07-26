ba3b840bd883d3f025fe691778edc8935fe03c04
yeatersink <163296549+yeatersink@users.noreply.github.com>
2025-07-26 06:59:13 +0300

    Updated name of link for search and content.

--- src/lib/utils/navManager.ts
+++ src/lib/utils/navManager.ts
@@ -1,9 +1,9 @@
 export const navItems: { name: string; url: string }[] = [
 	{ name: 'Home Page', url: '/' },
-	{ name: 'Content', url: '/content' },
+	{ name: 'Instructions and Information', url: '/content' },
 	// { name: 'Blockchain and Exchanges', url: '/block_chain' },
 	// { name: 'Digital Wallets', url: '/wallet' },
 	// { name: 'How to Get Started with Crypto', url: '/tools' },
-	{ name: 'Search', url: '/search' }
+	{ name: 'Search for a Coin', url: '/search' }
 	// { name: 'Liquidity and Liquidity Providing', url: '/liquidity' }
 ];
